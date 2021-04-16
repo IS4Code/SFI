0a4bb95d07e3e18933f9c83bf6864c58c61a7472
IS4 <illidans4@gmail.com>
2021-04-16 16:03:45 +0200

    archive media types

--- MultiArchiver.Formats/Archives/ArchiveFormat.cs
+++ MultiArchiver.Formats/Archives/ArchiveFormat.cs
@@ -1,5 +1,6 @@
 ﻿using IS4.MultiArchiver.Services;
 using SharpCompress.Archives;
+using Archives = SharpCompress.Archives;
 using System;
 using System.IO;
 
@@ -10,6 +11,35 @@ namespace IS4.MultiArchiver.Formats
         public ArchiveFormat() : base(0, null, null)
         {
 
+        }
+
+        private (string ext, string type) GetArchiveInfo(IArchive archive)
+        {
+            switch(archive)
+            {
+                case Archives.Rar.RarArchive _:
+                    return ("rar", "application/vnd.rar");
+                case Archives.Zip.ZipArchive _:
+                    return ("zip", "application/zip");
+                case Archives.Tar.TarArchive _:
+                    return ("tar", "application/x-tar");
+                case Archives.SevenZip.SevenZipArchive _:
+                    return ("7z", "application/x-7z-compressed");
+                case Archives.GZip.GZipArchive _:
+                    return ("gz", "application/gzip");
+                default:
+                    return default;
+            }
+        }
+
+        public override string GetExtension(IArchive value)
+        {
+            return GetArchiveInfo(value).ext;
+        }
+
+        public override string GetMediaType(IArchive value)
+        {
+            return GetArchiveInfo(value).type;
         }
 
         public override bool Match(Span<byte> header)
