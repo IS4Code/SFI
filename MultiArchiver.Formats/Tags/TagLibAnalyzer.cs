1b89ac0502b2e032ded8bcfea0bd506f5e9d6709
IS4 <illidans4@gmail.com>
2021-05-14 12:05:48 +0200

    basic tag analyzer

--- MultiArchiver.Formats/Tags/TagLibAnalyzer.cs
+++ MultiArchiver.Formats/Tags/TagLibAnalyzer.cs
@@ -1,13 +1,30 @@
 ﻿using IS4.MultiArchiver.Services;
+using System;
 using TagLib;
+using Properties = IS4.MultiArchiver.Vocabulary.Properties;
 
 namespace IS4.MultiArchiver.Analyzers
 {
-    public class TagLibAnalyzer : BinaryFormatAnalyzer<TagLib.File>
+    public class TagLibAnalyzer : BinaryFormatAnalyzer<File>
     {
-        public override bool Analyze(ILinkedNode node, File entity, ILinkedNodeFactory nodeFactory)
+        public override bool Analyze(ILinkedNode node, File file, ILinkedNodeFactory nodeFactory)
         {
-            return base.Analyze(node, entity, nodeFactory);
+            var properties = file.Properties;
+            Set(node, Properties.Width, properties.PhotoWidth);
+            Set(node, Properties.Height, properties.PhotoHeight);
+            Set(node, Properties.Width, properties.VideoWidth);
+            Set(node, Properties.Height, properties.VideoHeight);
+            Set(node, Properties.BitsPerSample, properties.BitsPerSample);
+            Set(node, Properties.SampleRate, properties.AudioSampleRate);
+            Set(node, Properties.Channels, properties.AudioChannels);
+            Set(node, Properties.Duration, properties.Duration);
+            return false;
+        }
+
+        private void Set<T>(ILinkedNode node, Properties prop, T valueOrDefault) where T : struct, IEquatable<T>, IFormattable
+        {
+            if(valueOrDefault.Equals(default)) return;
+            node.Set(prop, valueOrDefault);
         }
     }
 }
