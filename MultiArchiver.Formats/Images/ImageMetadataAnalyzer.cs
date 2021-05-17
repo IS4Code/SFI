ec7705c9bd3f275a530dac07df98e64dfa6e62ce
IS4 <illidans4@gmail.com>
2021-05-17 11:50:10 +0200

    turn conflicting directories to streams

--- MultiArchiver.Formats/Images/ImageMetadataAnalyzer.cs
+++ MultiArchiver.Formats/Images/ImageMetadataAnalyzer.cs
@@ -25,64 +25,87 @@ namespace IS4.MultiArchiver.Analyzers
                 {
                     result = s;
                 }
+            }
 
-                if(dir.Tags.FirstOrDefault(t => t.Name == "Width" || t.Name == "Image Width") is Tag width)
+            var streams = IdentifyTags(entity, "Width", "Image Width", "Height", "Image Height", "Bits Per Sample", "Channels", "Sampling Rate", "Duration").ToList();
+            
+            if(streams.Count > 0)
+            {
+                if(streams.Count == 1)
                 {
-                    if(dir.TryGetInt32(width.Type, out var value))
+                    Analyze(node, -1, streams[0].Key, streams[0].Value, entity, nodeFactory);
+                }else{
+                    for(int i = 0; i < streams.Count; i++)
                     {
-                        node.Set(Properties.Width, value);
-                    }
-                }
-                if(dir.Tags.FirstOrDefault(t => t.Name == "Height" || t.Name == "Image Height") is Tag height)
-                {
-                    if(dir.TryGetInt32(height.Type, out var value))
-                    {
-                        node.Set(Properties.Height, value);
-                    }
-                }
-                if(dir.Tags.FirstOrDefault(t => t.Name == "Bits Per Sample") is Tag bits)
-                {
-                    Properties prop;
-                    switch(entity.OfType<FileTypeDirectory>().FirstOrDefault()?.GetString(FileTypeDirectory.TagDetectedFileMimeType)?.Substring(0, 6).ToLowerInvariant())
-                    {
-                        case "audio/":
-                            prop = Properties.BitsPerSample;
-                            break;
-                        case "image/":
-                            prop = Properties.ColorDepth;
-                            break;
-                        default:
-                            prop = Properties.BitDepth;
-                            break;
-                    }
-                    if(dir.TryGetInt32(bits.Type, out var value))
-                    {
-                        node.Set(prop, value);
-                    }
-                }
-                if(dir.Tags.FirstOrDefault(t => t.Name == "Channels") is Tag channels)
-                {
-                    if(dir.TryGetInt32(channels.Type, out var value))
-                    {
-                        node.Set(Properties.Channels, value);
-                    }
-                }
-                if(dir.Tags.FirstOrDefault(t => t.Name == "Sampling Rate") is Tag rate)
-                {
-                    if(dir.TryGetInt32(rate.Type, out var value))
-                    {
-                        node.Set(Properties.SampleRate, value, Datatypes.Hertz);
-                    }
-                }
-                if(dir.Tags.FirstOrDefault(t => t.Name == "Duration") is Tag duration)
-                {
-                    if(TimeSpan.TryParse(dir.GetString(duration.Type), CultureInfo.InvariantCulture, out var value))
-                    {
-                        node.Set(Properties.Duration, value);
+                        var streamNode = Analyze(node, i, streams[0].Key, streams[0].Value, entity, nodeFactory);
+                        if(streamNode != null)
+                        {
+                            node.Set(Properties.HasMediaStream, streamNode);
+                        }
                     }
                 }
             }
+
             return result;
+        }
+
+        static IEnumerable<KeyValuePair<Directory, Dictionary<string, object>>> IdentifyTags(IReadOnlyList<Directory> entity, params string[] tagNames)
+        {
+            return entity.Select(d => new KeyValuePair<Directory, Dictionary<string, object>>(d, tagNames.Select(n => d.Tags.FirstOrDefault(t => t.Name == n)).Where(n => n != null).ToDictionary(t => t.Name, t => d.GetObject(t.Type))))
+                .Where(p => p.Value.Values.Any(v => v != null));
+        }
+
+        ILinkedNode Analyze(ILinkedNode parent, int index, Directory dir, IReadOnlyDictionary<string, object> tags, IReadOnlyList<Directory> entity, ILinkedNodeFactory nodeFactory)
+        {
+            var node = index >= 0 ? parent[index.ToString()] : parent;
+
+            if(tags.TryGetValue("Width", out var width) || tags.TryGetValue("Image Width", out width))
+            {
+                node.Set(Properties.Width, Convert.ToInt32(width));
+            }
+
+            if(tags.TryGetValue("Height", out var height) || tags.TryGetValue("Image Height", out height))
+            {
+                node.Set(Properties.Height, Convert.ToInt32(height));
+            }
+
+            if(tags.TryGetValue("Bits Per Sample", out var bits))
+            {
+                Properties prop;
+                switch(entity.OfType<FileTypeDirectory>().FirstOrDefault()?.GetString(FileTypeDirectory.TagDetectedFileMimeType)?.Substring(0, 6).ToLowerInvariant())
+                {
+                    case "audio/":
+                        prop = Properties.BitsPerSample;
+                        break;
+                    case "image/":
+                        prop = Properties.ColorDepth;
+                        break;
+                    default:
+                        prop = Properties.BitDepth;
+                        break;
+                }
+                node.Set(prop, Convert.ToInt32(bits));
+            }
+
+            if(tags.TryGetValue("Channels", out var channels))
+            {
+                node.Set(Properties.Channels, Convert.ToInt32(channels));
+            }
+
+            if(tags.TryGetValue("Sampling Rate", out var rate))
+            {
+                node.Set(Properties.Channels, Convert.ToInt32(rate), Datatypes.Hertz);
+            }
+
+            if(tags.TryGetValue("Duration", out var duration))
+            {
+                if(TimeSpan.TryParse(duration.ToString(), CultureInfo.InvariantCulture, out var value))
+                {
+                    node.Set(Properties.Duration, value);
+                }
+            }
+
+            return node;
         }
 
         public static ImageMetadataAnalyzer CreateDefault()
