2ba8b30fc6d771b43c3c75d13b73204960364ced
IS4 <illidans4@gmail.com>
2021-05-19 11:54:33 +0200

    fix bitmaps

--- MultiArchiver.Windows/NeReader.cs
+++ MultiArchiver.Windows/NeReader.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace IS4.MultiArchiver.Windows
@@ -121,15 +122,22 @@ namespace IS4.MultiArchiver.Windows
 
             public Resource(Stream stream, object type, object name, long offset, int length, int maxAlignment)
             {
+                var typeCode = type as Win32ResourceType? ?? 0;
+
                 ReferenceKey = stream;
                 Type = type;
                 Name = name;
                 Length = length;
-                var data = new byte[length];
+                int start = 0;
+                if(typeCode == Win32ResourceType.Bitmap)
+                {
+                    start = 14;
+                }
+                var data = new byte[start + length];
                 stream.Position = offset;
-                int read = stream.Read(data, 0, length);
-                int minLength = length - maxAlignment;
-                for(int i = read - 1; i >= minLength; i--)
+                int read = stream.Read(data, start, length);
+                int minLength = start + length - maxAlignment;
+                for(int i = start + read - 1; i >= minLength; i--)
                 {
                     if(data[i] != 0)
                     {
@@ -138,6 +146,29 @@ namespace IS4.MultiArchiver.Windows
                     }
                 }
                 Data = new ArraySegment<byte>(data, 0, minLength);
+                if(typeCode == Win32ResourceType.Bitmap)
+                {
+                    var header = new Span<byte>(data, 0, start);
+                    MemoryMarshal.Cast<byte, short>(header)[0] = 0x4D42;
+                    var fields = MemoryMarshal.Cast<byte, int>(header.Slice(2));
+                    fields[0] = Data.Count;
+
+                    int dataStart = start + BitConverter.ToInt32(data, start);
+
+                    int numColors = BitConverter.ToInt32(data, start + 32);
+                    if(numColors == 0)
+                    {
+                        var bits = BitConverter.ToInt16(data, start + 14);
+                        if(bits < 16)
+                        {
+                            numColors = 1 << bits;
+                        }
+                    }
+
+                    dataStart += numColors * sizeof(int);
+
+                    fields[2] = dataStart;
+                }
             }
 
             public Stream Open()
