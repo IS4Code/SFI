d73448b634447286277f7c08ade8bf09f05d6c44
IS4 <illidans4@gmail.com>
2021-05-28 18:54:24 +0200

    move to own file

--- MultiArchiver.Formats/Archives/SzReader.cs
+++ MultiArchiver.Formats/Archives/SzReader.cs
@@ -1,4 +1,5 @@
-﻿using SharpCompress.Common;
+﻿using IS4.MultiArchiver.Tools;
+using SharpCompress.Common;
 using SharpCompress.Readers;
 using System;
 using System.Collections.Generic;
@@ -83,16 +84,8 @@ namespace IS4.MultiArchiver.Formats.Archives
 
         public EntryStream OpenEntryStream()
         {
-            var stream = new EnumeratorStream<NoException>(ReadBytes(), Entry.Size);
+            var stream = new EnumeratorStream<EnumeratorStream<Exception>.NoException>(ReadBytes(), Entry.Size);
             return (EntryStream)ctor.Invoke(new object[] { this, stream });
-        }
-
-        abstract class NoException : Exception
-        {
-            private NoException()
-            {
-
-            }
         }
 
         const int windowSize = 4096;
@@ -206,92 +199,5 @@ namespace IS4.MultiArchiver.Formats.Archives
             public int? Attrib => null;
         }
 
-        class EnumeratorStream<TException> : Stream where TException : Exception
-        {
-            readonly IEnumerator<ArraySegment<byte>> enumerator;
-
-            public EnumeratorStream(IEnumerator<ArraySegment<byte>> enumerator, long length)
-            {
-                this.enumerator = enumerator;
-                Length = length;
-            }
-
-            public override bool CanRead => true;
-
-            public override bool CanSeek => false;
-
-            public override bool CanWrite => false;
-
-            public override long Length { get; }
-
-            public override long Position { get => throw new NotSupportedException(); set => throw new NotSupportedException(); }
-
-            public override void Flush()
-            {
-                
-            }
-
-            ArraySegment<byte> remaining;
-
-            public override int Read(byte[] buffer, int offset, int count)
-            {
-                int total = 0;
-                while(count > 0)
-                {
-                    while(remaining.Count == 0)
-                    {
-                        if(!MoveNext()) return total;
-                    }
-                    int read = Math.Min(remaining.Count, count);
-                    Array.Copy(remaining.Array, remaining.Offset, buffer, offset, read);
-                    remaining = new ArraySegment<byte>(remaining.Array, remaining.Offset + read, remaining.Count - read);
-                    total += read;
-                    offset += read;
-                    count -= read;
-                }
-                return total;
-            }
-
-            public override int ReadByte()
-            {
-                while(remaining.Count == 0)
-                {
-                    if(!MoveNext()) return -1;
-                }
-                var result = remaining.Array[remaining.Offset];
-                remaining = new ArraySegment<byte>(remaining.Array, remaining.Offset + 1, remaining.Count - 1);
-                return result;
-            }
-
-            bool MoveNext()
-            {
-                try{
-                    if(enumerator.MoveNext())
-                    {
-                        remaining = enumerator.Current;
-                        return true;
-                    }
-                    return false;
-                }catch(TException)
-                {
-                    return false;
-                }
-            }
-
-            public override long Seek(long offset, SeekOrigin origin)
-            {
-                throw new NotSupportedException();
-            }
-
-            public override void SetLength(long value)
-            {
-                throw new NotSupportedException();
-            }
-
-            public override void Write(byte[] buffer, int offset, int count)
-            {
-                throw new NotSupportedException();
-            }
-        }
     }
 }
---
+++ MultiArchiver/Tools/EnumeratorStream.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace IS4.MultiArchiver.Tools
+{
+    public class EnumeratorStream<TException> : Stream where TException : Exception
+    {
+        readonly IEnumerator<ArraySegment<byte>> enumerator;
+
+        public EnumeratorStream(IEnumerator<ArraySegment<byte>> enumerator, long length)
+        {
+            this.enumerator = enumerator;
+            Length = length;
+        }
+
+        public override bool CanRead => true;
+
+        public override bool CanSeek => false;
+
+        public override bool CanWrite => false;
+
+        public override long Length { get; }
+
+        public override long Position { get => throw new NotSupportedException(); set => throw new NotSupportedException(); }
+
+        public override void Flush()
+        {
+                
+        }
+
+        ArraySegment<byte> remaining;
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            int total = 0;
+            while(count > 0)
+            {
+                while(remaining.Count == 0)
+                {
+                    if(!MoveNext()) return total;
+                }
+                int read = Math.Min(remaining.Count, count);
+                Array.Copy(remaining.Array, remaining.Offset, buffer, offset, read);
+                remaining = new ArraySegment<byte>(remaining.Array, remaining.Offset + read, remaining.Count - read);
+                total += read;
+                offset += read;
+                count -= read;
+            }
+            return total;
+        }
+
+        public override int ReadByte()
+        {
+            while(remaining.Count == 0)
+            {
+                if(!MoveNext()) return -1;
+            }
+            var result = remaining.Array[remaining.Offset];
+            remaining = new ArraySegment<byte>(remaining.Array, remaining.Offset + 1, remaining.Count - 1);
+            return result;
+        }
+
+        bool MoveNext()
+        {
+            try{
+                if(enumerator.MoveNext())
+                {
+                    remaining = enumerator.Current;
+                    return true;
+                }
+                return false;
+            }catch(TException)
+            {
+                return false;
+            }
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            throw new NotSupportedException();
+        }
+
+        public abstract class NoException : Exception
+        {
+            private NoException()
+            {
+
+            }
+
+            public sealed override IDictionary Data => throw this;
+
+            public sealed override string HelpLink { get => throw this; set => throw this; }
+
+            public sealed override string Message => throw this;
+
+            public sealed override string Source { get => throw this; set => throw this; }
+
+            public sealed override string StackTrace => throw this;
+
+            public sealed override Exception GetBaseException()
+            {
+                throw this;
+            }
+        }
+    }
+}
