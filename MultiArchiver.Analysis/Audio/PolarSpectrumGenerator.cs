74350f4f6de6a7edfdc1e4e9362e7e0317c9b0cb
IS4 <illidans4@gmail.com>
2021-05-28 20:22:28 +0200

    prune namespaces

--- MultiArchiver.Analysis/Audio/PolarSpectrumGenerator.cs
+++ MultiArchiver.Analysis/Audio/PolarSpectrumGenerator.cs
@@ -1,12 +1,9 @@
 ﻿using NAudio.Wave;
 using System;
 using System.Buffers;
-using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.Numerics;
-using System.Runtime.InteropServices;
-using System.Text;
 
 namespace IS4.MultiArchiver.Analysis.Audio
 {
