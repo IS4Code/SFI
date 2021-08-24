63e5273dbd32dbffd4554a25cd56fc1c6bd25106
IS4 <illidans4@gmail.com>
2021-08-24 11:31:21 +0200

    skip SVG GDI+ check

--- MultiArchiver.Formats/Images/SvgFormat.cs
+++ MultiArchiver.Formats/Images/SvgFormat.cs
@@ -14,6 +14,11 @@ namespace IS4.MultiArchiver.Formats
             typeof(SvgDocument).GetMethod(nameof(SvgDocument.Open), BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic, null, new[] { typeof(XmlReader) }, null )
             .MakeGenericMethod(typeof(SvgDocument)));
 
+        static SvgFormat()
+        {
+            SvgDocument.SkipGdiPlusCapabilityCheck = true;
+        }
+
         public SvgFormat() : base("-//W3C//DTD SVG 1.1//EN", "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd", new Uri("http://www.w3.org/2000/svg", UriKind.Absolute), "image/svg+xml", "svg")
         {
 
