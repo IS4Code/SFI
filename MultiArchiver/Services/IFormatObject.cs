b3a9e3e67c892bc78514e748751fbf33a8040fbd
IS4 <illidans4@gmail.com>
2022-03-28 21:57:36 +0200

    return null

--- MultiArchiver/Services/IFormatObject.cs
+++ MultiArchiver/Services/IFormatObject.cs
@@ -62,7 +62,7 @@ namespace IS4.MultiArchiver.Services
                         return result;
                     }
                 }
-                if(String.IsNullOrEmpty(value.Authority)) return UriTools.CreateUuid(Guid.NewGuid());
+                if(String.IsNullOrEmpty(value.Authority)) return null;
                 var sub = Extension?.ToLowerInvariant();
                 if(sub == null)
                 {
