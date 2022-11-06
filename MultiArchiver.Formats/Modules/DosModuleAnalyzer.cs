6c601960b558a61de5ed881405df8880534283fd
IS4 <illidans4@gmail.com>
2022-11-06 21:41:00 +0100

    extract DOS Module class

--- MultiArchiver.Formats/Modules/DosModuleAnalyzer.cs
+++ MultiArchiver.Formats/Modules/DosModuleAnalyzer.cs
@@ -1,18 +1,14 @@
-﻿using IS4.MultiArchiver.Services;
-using IS4.MultiArchiver.Tools;
+﻿using IS4.MultiArchiver.Formats.Modules;
+using IS4.MultiArchiver.Services;
 using IS4.MultiArchiver.Vocabulary;
-using IS4.Tools;
-using System;
-using System.IO;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace IS4.MultiArchiver.Analyzers
 {
     /// <summary>
-    /// An analyzer of MS-DOS modules, as instances of <see cref="Module"/>.
+    /// An analyzer of MS-DOS modules, as instances of <see cref="DosModule"/>.
     /// </summary>
-    public class DosModuleAnalyzer : MediaObjectAnalyzer<DosModuleAnalyzer.Module>
+    public class DosModuleAnalyzer : MediaObjectAnalyzer<DosModule>
     {
         /// <inheritdoc cref="EntityAnalyzer.EntityAnalyzer"/>
         public DosModuleAnalyzer() : base(Common.ApplicationClasses)
@@ -20,7 +16,7 @@ namespace IS4.MultiArchiver.Analyzers
 
         }
 
-        public async override ValueTask<AnalysisResult> Analyze(Module module, AnalysisContext context, IEntityAnalyzers analyzers)
+        public async override ValueTask<AnalysisResult> Analyze(DosModule module, AnalysisContext context, IEntityAnalyzers analyzers)
         {
             var node = GetNode(context);
             var uncompressed = module.GetCompressedContents();
@@ -33,97 +29,6 @@ namespace IS4.MultiArchiver.Analyzers
                 }
             }
             return new AnalysisResult(node);
-        }
-
-        /// <summary>
-        /// Represents an MS-DOS executable module.
-        /// </summary>
-        public class Module
-        {
-            readonly BinaryReader reader;
-
-            /// <summary>
-            /// Creates a new instance of the module.
-            /// </summary>
-            /// <param name="stream">The stream storing the module in the MZ format.</param>
-            public Module(Stream stream)
-            {
-                reader = new BinaryReader(stream, Encoding.ASCII, true);
-
-                if(stream.Length < 0x3C + 4) return;
-                stream.Position = 0x3C;
-                var headerOffset = reader.ReadUInt32();
-                if(headerOffset <= 1 || headerOffset >= stream.Length - 2) return;
-                stream.Position = headerOffset;
-                var b = reader.ReadByte();
-                if(b < 0x41 || b > 0x5A) return;
-                b = reader.ReadByte();
-                if(b < 0x41 || b > 0x5A) return;
-                throw new ArgumentException("This file uses an extended executable format.", nameof(stream));
-            }
-
-            /// <summary>
-            /// Decompresses the executable.
-            /// </summary>
-            /// <returns>
-            /// The decompressed equivalent of the executable, as an instance
-            /// of <see cref="IFileInfo"/>.
-            /// </returns>
-            public IFileInfo GetCompressedContents()
-            {
-                var lzex = new LzExtractedFile(reader);
-                if(!lzex.Valid) return null;
-                return lzex;
-            }
-
-            class LzExtractedFile : LzExtractor, IFileInfo
-            {
-                readonly Lazy<ArraySegment<byte>> data;
-
-                public LzExtractedFile(BinaryReader reader) : base(reader)
-                {
-                    data = new Lazy<ArraySegment<byte>>(() => {
-                        var stream = Decompress();
-                        return stream.GetData();
-                    });
-                }
-
-                public bool IsEncrypted => false;
-
-                public string Name => null;
-
-                public string SubName => null;
-
-                public string Path => null;
-
-                public int? Revision => null;
-
-                public DateTime? CreationTime => null;
-
-                public DateTime? LastWriteTime => null;
-
-                public DateTime? LastAccessTime => null;
-
-                public long Length => data.Value.Count;
-
-                public StreamFactoryAccess Access => StreamFactoryAccess.Parallel;
-
-                public object ReferenceKey => this;
-
-                public object DataKey => null;
-
-                public FileKind Kind => FileKind.Embedded;
-
-                public Stream Open()
-                {
-                    return this.data.Value.AsStream(false);
-                }
-
-                public override string ToString()
-                {
-                    return "LZEXE-compressed executable";
-                }
-            }
         }
     }
 }
--- MultiArchiver.Formats/Modules/DosModuleFormat.cs
+++ MultiArchiver.Formats/Modules/DosModuleFormat.cs
@@ -1,4 +1,4 @@
-﻿using IS4.MultiArchiver.Analyzers;
+﻿using IS4.MultiArchiver.Formats.Modules;
 using IS4.MultiArchiver.Tools;
 using System;
 using System.IO;
@@ -8,9 +8,9 @@ namespace IS4.MultiArchiver.Formats
 {
     /// <summary>
     /// Represents the MS-DOS MZ module format, producing instances of
-    /// <see cref="DosModuleAnalyzer.Module"/>.
+    /// <see cref="DosModule"/>.
     /// </summary>
-    public class DosModuleFormat : ModuleFormat<DosModuleAnalyzer.Module>
+    public class DosModuleFormat : ModuleFormat<DosModule>
     {
         /// <inheritdoc cref="FileFormat{T}.FileFormat(string, string)"/>
         public DosModuleFormat() : base(null, "application/x-dosexec", "exe")
@@ -24,9 +24,9 @@ namespace IS4.MultiArchiver.Formats
             return (fields.Length > 0 && fields[0] == 0x4D5A) || base.CheckSignature(header);
         }
 
-        public async override ValueTask<TResult> Match<TResult, TArgs>(Stream stream, MatchContext context, ResultFactory<DosModuleAnalyzer.Module, TResult, TArgs> resultFactory, TArgs args)
+        public async override ValueTask<TResult> Match<TResult, TArgs>(Stream stream, MatchContext context, ResultFactory<DosModule, TResult, TArgs> resultFactory, TArgs args)
         {
-            return await resultFactory(new DosModuleAnalyzer.Module(stream), args);
+            return await resultFactory(new DosModule(stream), args);
         }
     }
 }
