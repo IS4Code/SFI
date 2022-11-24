8c0ea38334add96ba5da136d86370a0f3b8c5af9
IS4 <illidans4@gmail.com>
2022-11-25 00:46:34 +0100

    emulate DOS executables

--- SFI.ExternalFormats/Modules/DosModule.cs
+++ SFI.ExternalFormats/Modules/DosModule.cs
@@ -1,8 +1,13 @@
-﻿using IS4.SFI.Services;
+﻿using Aeon.Emulator;
+using Aeon.Emulator.Dos.Programs;
+using Aeon.Emulator.Dos.VirtualFileSystem;
+using Aeon.Emulator.Video;
+using IS4.SFI.Services;
 using IS4.SFI.Tools;
 using IS4.Tools;
 using System;
 using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace IS4.SFI.Formats.Modules
@@ -32,6 +37,86 @@ namespace IS4.SFI.Formats.Modules
             b = reader.ReadByte();
             if(b < 0x41 || b > 0x5A) return;
             throw new ArgumentException("This file uses an extended executable format.", nameof(stream));
+        }
+
+        public string? Emulate(Encoding consoleEncoding, int step, int max)
+        {
+            using var machine = new VirtualMachine();
+            machine.EndInitialization();
+
+            var video = machine.Devices.OfType<VideoHandler>().FirstOrDefault();
+            if(video == null)
+            {
+                return null;
+            }
+
+            var stream = reader.BaseStream;
+            stream.Position = 0;
+            var image = new ExeFile(new VirtualPath(), stream);
+            machine.LoadImage(image);
+
+            EventHandler endHandler = delegate { throw new EndOfProgramException(); };
+            machine.VideoModeChanged += endHandler;
+            machine.CurrentProcessChanged += endHandler;
+
+            int instructions = 0;
+            while(true)
+            {
+                try{
+                    machine.Emulate(step);
+                    instructions += step;
+                    if(instructions > max)
+                    {
+                        break;
+                    }
+                }catch(EndOfProgramException)
+                { 
+                    break;
+                }catch(NotImplementedException)
+                {
+                    break;
+                }catch(NotSupportedException)
+                {
+                    break;
+                }
+            }
+
+            machine.VideoModeChanged -= endHandler;
+            machine.CurrentProcessChanged -= endHandler;
+
+            var console = video.TextConsole;
+            var plane = console.GetPlaneData(0);
+            return ReadTextMemory(plane, console.Width, console.Height, consoleEncoding);
+        }
+
+        string? ReadTextMemory(Span<byte> memory, int width, int height, Encoding encoding)
+        {
+            StringBuilder? sb = null;
+            int numLines = 0;
+            for(int y = 0; y < height; y++)
+            {
+                var row = memory.Slice(y * width, width);
+                var end = row.IndexOf((byte)0);
+                if(end != 0)
+                {
+                    if(end != -1)
+                    {
+                        row = row.Slice(0, end);
+                    }
+                    if(numLines > 0)
+                    {
+                        sb ??= new();
+                        for(int i = 0; i < numLines; i++)
+                        {
+                            sb.Append('\n');
+                        }
+                        numLines = 0;
+                    }
+                    (sb ??= new()).Append(encoding.GetString(row.ToArray()));
+                }
+                numLines++;
+            }
+            return sb?.ToString();
         }
 
         /// <summary>
--- SFI.ExternalFormats/Modules/DosModuleAnalyzer.cs
+++ SFI.ExternalFormats/Modules/DosModuleAnalyzer.cs
@@ -1,6 +1,7 @@
 ﻿using IS4.SFI.Formats.Modules;
 using IS4.SFI.Services;
 using IS4.SFI.Vocabulary;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace IS4.SFI.Analyzers
@@ -10,10 +11,35 @@ namespace IS4.SFI.Analyzers
     /// </summary>
     public class DosModuleAnalyzer : MediaObjectAnalyzer<DosModule>
     {
+        /// <summary>
+        /// Contains the encoding used to decode texts produced by the module.
+        /// </summary>
+        public Encoding ConsoleEncoding { get; set; }
+
+        /// <summary>
+        /// Whether to attempt to run the executable to obtain additional data.
+        /// </summary>
+        public bool Emulate { get; set; } = true;
+
+        /// <summary>
+        /// The number of instruction to emulate in each step.
+        /// </summary>
+        public int InstructionStep { get; set; } = 1024;
+
+        /// <summary>
+        /// The limit on total number of instructions after which
+        /// emulation is stopped.
+        /// </summary>
+        public int InstructionLimit { get; set; } = 1048576;
+
         /// <inheritdoc cref="EntityAnalyzer.EntityAnalyzer"/>
         public DosModuleAnalyzer() : base(Common.ApplicationClasses)
         {
-
+            try{
+                ConsoleEncoding = Encoding.GetEncoding(437);
+            }catch{
+                ConsoleEncoding = Encoding.ASCII;
+            }
         }
 
         /// <inheritdoc/>
@@ -27,6 +53,13 @@ namespace IS4.SFI.Analyzers
                 if(infoNode != null)
                 {
                     node.Set(Properties.BelongsToContainer, infoNode);
+                }
+            }else if(Emulate)
+            {
+                var text = module.Emulate(ConsoleEncoding, InstructionStep, InstructionLimit);
+                if(text != null)
+                {
+                    node.Set(Properties.Description, text);
                 }
             }
             return new AnalysisResult(node);
