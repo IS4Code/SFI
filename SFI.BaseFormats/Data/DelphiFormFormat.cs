abcba6064d414ab9c836fa58602da22bd8065f61
IS4 <illidans4@gmail.com>
2022-11-27 17:13:38 +0100

    configurable encoding

--- SFI.BaseFormats/Data/DelphiFormFormat.cs
+++ SFI.BaseFormats/Data/DelphiFormFormat.cs
@@ -10,17 +10,37 @@ namespace IS4.SFI.Formats
     /// </summary>
     public class DelphiFormFormat : SignatureFormat<DelphiObject>
     {
+        /// <summary>
+        /// Contains the encoding used for reading strings.
+        /// </summary>
+        public Encoding DefaultEncoding { get; set; }
+
+        /// <summary>
+        /// Contains the name of <see cref="DefaultEncoding"/>.
+        /// </summary>
+        public string DefaultEncodingName {
+            get {
+                return DefaultEncoding.WebName;
+            }
+            set {
+                DefaultEncoding = Encoding.GetEncoding(value);
+            }
+        }
+
         /// <inheritdoc cref="FileFormat{T}.FileFormat(string, string)"/>
         public DelphiFormFormat() : base("TPF0", "application/x-delphi-form", "dfm")
         {
-
+            try{
+                DefaultEncoding = Encoding.GetEncoding(1252);
+            }catch{
+                DefaultEncoding = Encoding.ASCII;
+            }
         }
 
         /// <inheritdoc/>
         public async override ValueTask<TResult?> Match<TResult, TArgs>(Stream stream, MatchContext context, ResultFactory<DelphiObject, TResult, TArgs> resultFactory, TArgs args) where TResult : default
         {
-            var encoding = Encoding.GetEncoding(1252); //TODO: guess from context
-            return await resultFactory(DelphiFormReader.Read(stream, encoding), args);
+            return await resultFactory(DelphiFormReader.Read(stream, DefaultEncoding), args);
         }
     }
 }
