63b80ec96c35f316f7f17fa634c3f23c04d3764c
IS4 <illidans4@gmail.com>
2023-02-08 13:59:46 +0100

    new samples files and descriptions

---
+++ SFI.Tests/GraphTests.Samples.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+#pragma warning disable 1591
+
+namespace IS4.SFI.Tests
+{
+    partial class GraphTests
+    {
+        [DataRow("Samples/lnk/notepad.exe.lnk")]
+        [DataRow("Samples/lnk/ProgramFiles(x86).lnk")]
+        [DataRow("Samples/lnk/ExpandedVariables.lnk")]
+        [TestMethod]
+        public Task lnk(string source)
+        {
+            return TestOutputGraph(source);
+        }
+
+        [DataRow("Samples/url/Google.url")]
+        [TestMethod]
+        public Task url(string source)
+        {
+            return TestOutputGraph(source);
+        }
+
+        [DataRow("Samples/rdfxml/rdf.xml")]
+        [TestMethod]
+        public Task rdfxml(string source)
+        {
+            return TestOutputGraph(source);
+        }
+    }
+}
