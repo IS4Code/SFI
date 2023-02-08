74db354cc8492cf8febd26a392a0c99e2a0838f9
IS4 <illidans4@gmail.com>
2023-02-08 15:35:16 +0100

    more tests for various classes

---
+++ SFI.Tests/UnitTests/BitmapDataStream.cs
@@ -0,0 +1,74 @@
+﻿using IS4.SFI.Tools.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace IS4.SFI.Tests
+{
+    /// <summary>
+    /// The tests for <see cref="BitmapDataStream"/>.
+    /// </summary>
+    [TestClass]
+    public class BitmapDataStreamTests
+    {
+        /// <summary>
+        /// The tests for <see cref="BitmapDataStream.Read(byte[], int, int)"/>,
+        /// checking randomly-generated data and both positive and negative stride.
+        /// </summary>
+        [DataRow(0, false)]
+        [DataRow(402927094, false)]
+        [DataRow(452854933, false)]
+        [DataRow(1955186690, false)]
+        [DataRow(862248498, false)]
+        [DataRow(2112741252, false)]
+        [DataRow(1363995148, false)]
+        [DataRow(2093213676, false)]
+        [DataRow(503564212, false)]
+        [DataRow(0, true)]
+        [DataRow(402927094, true)]
+        [DataRow(452854933, true)]
+        [DataRow(1955186690, true)]
+        [DataRow(862248498, true)]
+        [DataRow(2112741252, true)]
+        [DataRow(1363995148, true)]
+        [DataRow(2093213676, true)]
+        [DataRow(503564212, true)]
+        [TestMethod]
+        public void ReadTests(int seed, bool negativeStride)
+        {
+            var rnd = new Random(seed);
+
+            var height = rnd.Next(1, 200);
+            var width = rnd.Next(1, 200);
+            var stride = (width + rnd.Next(0, 100)) * (negativeStride ? -1 : 1);
+
+            var data = new byte[width * height];
+            rnd.NextBytes(data);
+
+            var ptr = Marshal.AllocHGlobal(Math.Abs(stride) * height);
+            try
+            {
+                var scan0 = ptr + (negativeStride
+                    ? -stride * (height - 1)
+                    : 0);
+
+                for(int i = 0; i < height; i++)
+                {
+                    Marshal.Copy(data, i * width, scan0 + i * stride, width);
+                }
+
+                using(var stream = new BitmapDataStream(scan0, stride, height, width))
+                {
+                    using var buffer = new MemoryStream();
+                    stream.CopyTo(buffer);
+
+                    CollectionAssert.AreEqual(data, buffer.ToArray());
+                }
+            } finally
+            {
+                Marshal.FreeHGlobal(ptr);
+            }
+        }
+    }
+}
---
+++ SFI.Tests/UnitTests/ChannelXmlReaderTests.cs
@@ -0,0 +1,119 @@
+﻿using IS4.SFI.Tools.Xml;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace IS4.SFI.Tests
+{
+    /// <summary>
+    /// The tests for <see cref="ChannelXmlReader"/> and <see cref="XmlReaderState"/>.
+    /// </summary>
+    [TestClass]
+    public class ChannelXmlReaderTests
+    {
+        const string sampleXml = @"<?xml version=""1.0""?>
+<!-- comment -->
+<element attribute=""value"">
+  Text content
+  <element2 xmlns=""urn:publicid:test"" xmlns:other=""urn:publicid:other"" other:attr=""value2"">
+    <?pi content "" ""?>
+    <!-- comment2 -->
+  </element2>
+</element>
+";
+
+        /// <summary>
+        /// Tests that <see cref="ChannelXmlReader"/> produced from
+        /// a sequence of <see cref="XmlReaderState"/> results
+        /// in equivalent XML.
+        /// </summary>
+        [DataRow(sampleXml)]
+        [TestMethod]
+        public async Task ReconstructedXmlEquivalent(string xml)
+        {
+            ChannelXmlReader channelReader;
+
+            using(var reader = XmlReader.Create(new StringReader(xml)))
+            {
+                channelReader = ChannelXmlReader.Create(reader, out var writer);
+
+                foreach(var state in XmlReaderState.ReadFrom(reader))
+                {
+                    await writer.WriteAsync(state);
+                }
+
+                writer.Complete();
+            }
+
+            var expected = XDocument.Parse(xml, LoadOptions.PreserveWhitespace);
+            var actual = XDocument.Load(channelReader, LoadOptions.SetLineInfo);
+            Assert.AreEqual(expected.ToString(), actual.ToString());
+        }
+
+        /// <summary>
+        /// Tests that <see cref="ChannelXmlReader"/> produced from
+        /// a sequence of <see cref="XmlReaderState"/> results
+        /// in equivalent XML, using asynchronous reading of XML.
+        /// </summary>
+        [DataRow(sampleXml)]
+        [TestMethod]
+        public async Task ReconstructedXmlEquivalentAsync(string xml)
+        {
+            ChannelXmlReader channelReader;
+
+            using(var reader = XmlReader.Create(new StringReader(xml)))
+            {
+                channelReader = ChannelXmlReader.Create(reader, out var writer);
+
+                foreach(var state in XmlReaderState.ReadFrom(reader))
+                {
+                    await writer.WriteAsync(state);
+                }
+
+                writer.Complete();
+            }
+
+            var expected = XDocument.Parse(xml, LoadOptions.PreserveWhitespace);
+            var actual = await XDocument.LoadAsync(channelReader, LoadOptions.SetLineInfo, CancellationToken.None);
+            Assert.AreEqual(expected.ToString(), actual.ToString());
+        }
+
+        /// <summary>
+        /// Tests that <see cref="XmlReaderState.ReadFrom(XmlReader)"/> produces
+        /// a correct sequence of nodes based on their types.
+        /// </summary>
+        [DataRow(sampleXml, new []{
+            XmlNodeType.XmlDeclaration,
+            XmlNodeType.Whitespace,
+            XmlNodeType.Comment,
+            XmlNodeType.Whitespace,
+            XmlNodeType.Element,
+            XmlNodeType.Text,
+            XmlNodeType.Element,
+            XmlNodeType.Whitespace,
+            XmlNodeType.ProcessingInstruction,
+            XmlNodeType.Whitespace,
+            XmlNodeType.Comment,
+            XmlNodeType.Whitespace,
+            XmlNodeType.EndElement,
+            XmlNodeType.Whitespace,
+            XmlNodeType.EndElement,
+            XmlNodeType.Whitespace,
+            XmlNodeType.None
+        })]
+        [TestMethod]
+        public void ReadFromTests(string xml, XmlNodeType[] nodeTypes)
+        {
+            using(var reader = XmlReader.Create(new StringReader(xml)))
+            {
+                var types = XmlReaderState.ReadFrom(reader).Select(state => state.NodeType).ToList();
+
+                CollectionAssert.AreEqual(nodeTypes, types);
+            }
+        }
+    }
+}
