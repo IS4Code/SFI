df98adf0e6d5ab4e6dbf2f0b85a58331a9c19fa8
IS4 <illidans4@gmail.com>
2023-03-04 09:37:59 +0100

    doc fix

--- SFI/InternalApplicationException.cs
+++ SFI/InternalApplicationException.cs
@@ -4,7 +4,7 @@ using System.Runtime.Serialization;
 namespace IS4.SFI
 {
     /// <summary>
-    /// Stores contains an exception in <see cref="Exception.InnerException"/> that
+    /// Stores an exception in <see cref="Exception.InnerException"/> that
     /// should not be suppressed when the situation would normally be recoverable
     /// (such as when a format cannot be matched), indicating that a failure
     /// occured within the application itself.
