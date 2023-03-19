54ba100d2dde3f904020682e591f5bc69c60af94
IS4 <illidans4@gmail.com>
2023-03-19 15:05:35 +0100

    uri-list samples

---
+++ SFI.SamplePlugin/UriListAnalyzer.cs
@@ -0,0 +1,27 @@
+﻿using IS4.SFI.Services;
+using IS4.SFI.Vocabulary;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace IS4.SFI.Analyzers
+{
+    /// <summary>
+    /// An analyzer for the text/uri-list format, as a list of <see cref="Uri"/> instances.
+    /// </summary>
+    public class UriListAnalyzer : EntityAnalyzer<IReadOnlyList<Uri>>
+    {
+        /// <inheritdoc/>
+        public override async ValueTask<AnalysisResult> Analyze(IReadOnlyList<Uri> entity, AnalysisContext context, IEntityAnalyzers analyzers)
+        {
+            var node = GetNode(context);
+
+            for(int i = 0; i < entity.Count; i++)
+            {
+                node.Set(Properties.MemberAt, i + 1, entity[i]);
+            }
+
+            return new(node);
+        }
+    }
+}
---
+++ SFI.SamplePlugin/UriListFormat.cs
@@ -0,0 +1,49 @@
+﻿using IS4.SFI.Services;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IS4.SFI.Formats
+{
+    /// <summary>
+    /// The text/uri-list format, containing a list of <see cref="Uri"/> instances.
+    /// </summary>
+    public class UriListFormat : BinaryFileFormat<IReadOnlyList<Uri>>
+    {
+        /// <inheritdoc cref="FileFormat{T}.FileFormat(string, string)"/>
+        public UriListFormat() : base(246, "text/uri-list", "uris")
+        {
+
+        }
+
+        /// <inheritdoc/>
+        public override bool CheckHeader(ReadOnlySpan<byte> header, bool isBinary, IEncodingDetector? encodingDetector)
+        {
+            if(isBinary) return false;
+            using var reader = new StringReader(Encoding.UTF8.GetString(header));
+            while(reader.ReadLine() is string line)
+            {
+                if(Uri.TryCreate(line, UriKind.Absolute, out _))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <inheritdoc/>
+        public override async ValueTask<TResult?> Match<TResult, TArgs>(Stream stream, MatchContext context, ResultFactory<IReadOnlyList<Uri>, TResult, TArgs> resultFactory, TArgs args) where TResult : default
+        {
+            using var reader = new StreamReader(stream);
+            var list = new List<Uri>();
+            while(await reader.ReadLineAsync() is string line)
+            {
+                if(line.StartsWith("#")) continue;
+                list.Add(new Uri(line, UriKind.Absolute));
+            }
+            return await resultFactory(list, args);
+        }
+    }
+}
