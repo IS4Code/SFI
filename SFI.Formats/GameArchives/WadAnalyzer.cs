5ef425f0ddecc0e3aa18bb59d40b9eadf0c1bf08
IS4 <illidans4@gmail.com>
2023-12-03 00:42:43 +0100

    WAD format

---
+++ SFI.Formats/GameArchives/WadAnalyzer.cs
@@ -0,0 +1,91 @@
+﻿using IS4.SFI.Formats;
+using IS4.SFI.Services;
+using nz.doom.WadParser;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IS4.SFI.Analyzers
+{
+    /// <summary>
+    /// Analyzes WAD archives, as instances of <see cref="Wad"/>.
+    /// The analysis itself is performed by analyzing an
+    /// <see cref="IArchiveFile"/> adapter from the instance.
+    /// </summary>
+    [Description("Analyzes WAD archives.")]
+    public class WadAnalyzer : MediaObjectAnalyzer<Wad>
+    {
+        /// <inheritdoc/>
+        public async override ValueTask<AnalysisResult> Analyze(Wad wad, AnalysisContext context, IEntityAnalyzers analyzers)
+        {
+            return await analyzers.Analyze(new Adapter(wad), context);
+        }
+
+        class Adapter : IArchiveFile
+        {
+            readonly Wad wad;
+
+            public Adapter(Wad wad)
+            {
+                this.wad = wad;
+            }
+
+            public IEnumerable<IArchiveEntry> Entries => wad.Lumps.Select(l => new Entry(l));
+
+            public bool IsComplete => true;
+
+            public bool IsSolid => false;
+
+            class Entry : IArchiveEntry, IFileInfo
+            {
+                readonly Lump lump;
+
+                public Entry(Lump lump)
+                {
+                    this.lump = lump;
+                }
+
+                public DateTime? ArchivedTime => null;
+
+                public string? Name => lump.Name;
+
+                public string? SubName => null;
+
+                public string? Path => Name;
+
+                public int? Revision => null;
+
+                public DateTime? CreationTime => null;
+
+                public DateTime? LastWriteTime => null;
+
+                public DateTime? LastAccessTime => null;
+
+                public FileKind Kind => FileKind.ArchiveItem;
+
+                public FileAttributes Attributes => lump.IsCompressed ? FileAttributes.Compressed : FileAttributes.Normal;
+
+                public object? ReferenceKey => lump;
+
+                public object? DataKey => null;
+
+                public long Length => lump.Size;
+
+                public StreamFactoryAccess Access => StreamFactoryAccess.Parallel;
+
+                public Stream Open()
+                {
+                    return new MemoryStream(lump.Bytes, false);
+                }
+
+                public override string ToString()
+                {
+                    return "/" + Name;
+                }
+            }
+        }
+    }
+}
---
+++ SFI.Formats/GameArchives/WadFormat.cs
@@ -0,0 +1,57 @@
+﻿using IS4.SFI.Services;
+using nz.doom.WadParser;
+using System;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IS4.SFI.Formats
+{
+    /// <summary>
+    /// Represents the WAD archive format, as an instance of <see cref="Wad"/>.
+    /// </summary>
+    [Description("Represents the WAD archive format used in Doom and other games.")]
+    public class WadFormat : BinaryFileFormat<Wad>
+    {
+        static readonly byte[][] signatures = new[] { "IWAD", "PWAD", "WAD" }.Select(h => Encoding.ASCII.GetBytes(h)).ToArray();
+
+        /// <inheritdoc cref="FileFormat{T}.FileFormat(string, string)"/>
+        public WadFormat() : base(5, "application/x-doom", "wad")
+        {
+
+        }
+
+        /// <inheritdoc/>
+        public override bool CheckHeader(ReadOnlySpan<byte> header, bool isBinary, IEncodingDetector? encodingDetector)
+        {
+            if(header.Length < 5)
+            {
+                return false;
+            }
+            foreach(var sig in signatures)
+            {
+                if(header.StartsWith(sig.AsSpan()))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public override async ValueTask<TResult?> Match<TResult, TArgs>(Stream stream, MatchContext context, ResultFactory<Wad, TResult, TArgs> resultFactory, TArgs args) where TResult : default
+        {
+            if(stream is FileStream fileStream)
+            {
+                return await resultFactory(WadParser.Parse(fileStream), args);
+            }
+            using var tmpPath = FileTools.GetTemporaryFile("wad");
+            using(var file = new FileStream(tmpPath, FileMode.CreateNew))
+            {
+                stream.CopyTo(file);
+            }
+            return await resultFactory(WadParser.Parse(tmpPath), args);
+        }
+    }
+}
