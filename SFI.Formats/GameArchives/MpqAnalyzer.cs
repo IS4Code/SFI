559dc265ee35ce797204aba8ac8fa55b408cab47
IS4 <illidans4@gmail.com>
2023-12-05 14:12:06 +0100

    MPQ format

---
+++ SFI.Formats/GameArchives/MpqAnalyzer.cs
@@ -0,0 +1,110 @@
+﻿using IS4.SFI.Formats;
+using IS4.SFI.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using War3Net.IO.Mpq;
+
+namespace IS4.SFI.Analyzers
+{
+    /// <summary>
+    /// Analyzes MPQ archives, as instances of <see cref="MpqArchive"/>.
+    /// The analysis itself is performed by analyzing an
+    /// <see cref="IArchiveFile"/> adapter from the instance.
+    /// </summary>
+    [Description("Analyzes MPQ archives.")]
+    public class MpqAnalyzer : MediaObjectAnalyzer<MpqArchive>
+    {
+        /// <inheritdoc/>
+        public async override ValueTask<AnalysisResult> Analyze(MpqArchive wad, AnalysisContext context, IEntityAnalyzers analyzers)
+        {
+            return await analyzers.Analyze(new Adapter(wad), context);
+        }
+
+        class Adapter : IArchiveFile
+        {
+            readonly MpqArchive mpq;
+
+            public Adapter(MpqArchive mpq)
+            {
+                this.mpq = mpq;
+            }
+
+            public IEnumerable<IArchiveEntry> Entries => mpq.Select(e => new Entry(mpq, e));
+
+            public bool IsComplete => true;
+
+            public bool IsSolid => false;
+
+            class Entry : IArchiveEntry, IFileInfo
+            {
+                readonly MpqArchive mpq;
+                readonly MpqEntry entry;
+
+                public Entry(MpqArchive mpq, MpqEntry entry)
+                {
+                    this.mpq = mpq;
+                    this.entry = entry;
+                }
+
+                public DateTime? ArchivedTime => null;
+
+                public string? Name => entry.FileName;
+
+                public string? SubName => null;
+
+                public string? Path => Name;
+
+                public int? Revision => null;
+
+                public DateTime? CreationTime => null;
+
+                public DateTime? LastWriteTime => null;
+
+                public DateTime? LastAccessTime => null;
+
+                public FileKind Kind => FileKind.ArchiveItem;
+
+                public FileAttributes Attributes {
+                    get {
+                        FileAttributes attributes = 0;
+                        if(entry.IsCompressed)
+                        {
+                            attributes |= FileAttributes.Compressed;
+                        }
+                        if(entry.IsEncrypted)
+                        {
+                            attributes |= FileAttributes.Encrypted;
+                        }
+                        if(attributes == 0)
+                        {
+                            return FileAttributes.Normal;
+                        }
+                        return attributes;
+                    }
+                }
+
+                public object? ReferenceKey => entry;
+
+                public object? DataKey => null;
+
+                public long Length => entry.FileSize;
+
+                public StreamFactoryAccess Access => StreamFactoryAccess.Reentrant;
+
+                public Stream Open()
+                {
+                    return mpq.OpenFile(entry);
+                }
+
+                public override string ToString()
+                {
+                    return "/" + Name;
+                }
+            }
+        }
+    }
+}
---
+++ SFI.Formats/GameArchives/MpqFormat.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.IO;
+using System.Threading.Tasks;
+using War3Net.IO.Mpq;
+
+namespace IS4.SFI.Formats
+{
+    /// <summary>
+    /// Represents the MPQ archive format, as an instance of <see cref="MpqArchive"/>.
+    /// </summary>
+    [Description("Represents the MPQ archive format used in Blizzard games.")]
+    public class MpqFormat : SignatureFormat<MpqArchive>
+    {
+        /// <inheritdoc cref="FileFormat{T}.FileFormat(string, string)"/>
+        public MpqFormat() : base("MPQ", "application/x-mpq-compressed", "mpq")
+        {
+
+        }
+
+        public override async ValueTask<TResult?> Match<TResult, TArgs>(Stream stream, MatchContext context, ResultFactory<MpqArchive, TResult, TArgs> resultFactory, TArgs args) where TResult : default
+        {
+            using var archive = new MpqArchive(stream, loadListFile: true);
+            return await resultFactory(archive, args);
+        }
+    }
+}
