544ac956251a496f4d875f43ede21f46fb018789
IS4 <illidans4@gmail.com>
2023-12-05 12:10:07 +0100

    PBO format

---
+++ SFI.Formats/GameArchives/PboAnalyzer.cs
@@ -0,0 +1,96 @@
+﻿using BisUtils.PBO;
+using BisUtils.PBO.Builders;
+using BisUtils.PBO.Entries;
+using BisUtils.PBO.Interfaces;
+using IS4.SFI.Formats;
+using IS4.SFI.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IS4.SFI.Analyzers
+{
+    /// <summary>
+    /// Analyzes PBO archives, as instances of <see cref="IPboFile"/>.
+    /// The analysis itself is performed by analyzing an
+    /// <see cref="IArchiveFile"/> adapter from the instance.
+    /// </summary>
+    [Description("Analyzes PBO archives.")]
+    public class PboAnalyzer : MediaObjectAnalyzer<IPboFile>
+    {
+        /// <inheritdoc/>
+        public async override ValueTask<AnalysisResult> Analyze(IPboFile pbo, AnalysisContext context, IEntityAnalyzers analyzers)
+        {
+            return await analyzers.Analyze(new Adapter(pbo), context);
+        }
+
+        class Adapter : IArchiveFile
+        {
+            readonly IPboFile pbo;
+
+            public Adapter(IPboFile pbo)
+            {
+                this.pbo = pbo;
+            }
+
+            public IEnumerable<IArchiveEntry> Entries => pbo.GetPboEntries().OfType<PboDataEntry>().Select(e => new Entry(pbo, e));
+
+            public bool IsComplete => true;
+
+            public bool IsSolid => false;
+
+            class Entry : IArchiveEntry, IFileInfo
+            {
+                readonly PboDataEntry entry;
+                readonly Lazy<byte[]> data;
+
+                public Entry(IPboFile pbo, PboDataEntry entry)
+                {
+                    this.entry = entry;
+                    data = new Lazy<byte[]>(() => pbo.GetEntryData(entry));
+                }
+
+                public DateTime? ArchivedTime => null;
+
+                public string? Name => entry.EntryName;
+
+                public string? SubName => null;
+
+                public string? Path => Name;
+
+                public int? Revision => null;
+
+                public DateTime? CreationTime => null;
+
+                public DateTime? LastWriteTime => entry.TimeStamp is 0 or 1 ? null : DateTimeOffset.FromUnixTimeSeconds(unchecked((long)entry.TimeStamp)).DateTime;
+
+                public DateTime? LastAccessTime => null;
+
+                public FileKind Kind => FileKind.ArchiveItem;
+
+                public FileAttributes Attributes => entry.PackedSize != entry.OriginalSize ? FileAttributes.Compressed : FileAttributes.Normal;
+
+                public object? ReferenceKey => entry;
+
+                public object? DataKey => null;
+
+                public long Length => unchecked((long)entry.OriginalSize);
+
+                public StreamFactoryAccess Access => StreamFactoryAccess.Parallel;
+
+                public Stream Open()
+                {
+                    return new MemoryStream(data.Value, false);
+                }
+
+                public override string ToString()
+                {
+                    return "/" + Name;
+                }
+            }
+        }
+    }
+}
---
+++ SFI.Formats/GameArchives/PboFormat.cs
@@ -0,0 +1,31 @@
+﻿using BisUtils.PBO;
+using System.ComponentModel;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace IS4.SFI.Formats
+{
+    /// <summary>
+    /// Represents the PBO archive format, as an instance of <see cref="PboFile"/>.
+    /// </summary>
+    [Description("Represents the PBO archive format used in Bohemia Interactive games.")]
+    public class PboFormat : SignatureFormat<PboFile>
+    {
+        /// <inheritdoc cref="FileFormat{T}.FileFormat(string, string)"/>
+        public PboFormat() : base("\0sreV", "application/x-pbo", "pbo")
+        {
+
+        }
+
+        public override async ValueTask<TResult?> Match<TResult, TArgs>(Stream stream, MatchContext context, ResultFactory<PboFile, TResult, TArgs> resultFactory, TArgs args) where TResult : default
+        {
+            using var tmpPath = FileTools.GetTemporaryFile("pbo");
+            using(var file = new FileStream(tmpPath, FileMode.CreateNew))
+            {
+                stream.CopyTo(file);
+            }
+            using var pbo = new PboFile(tmpPath);
+            return await resultFactory(pbo, args);
+        }
+    }
+}
