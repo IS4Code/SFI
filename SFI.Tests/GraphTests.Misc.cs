adffb0e72d950280bca5d409111d0a60c4df13f2
IS4 <illidans4@gmail.com>
2023-12-05 16:32:50 +0100

    PBO samples test

---
+++ SFI.Tests/GraphTests.Misc.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+#pragma warning disable 1591
+
+namespace IS4.SFI.Tests
+{
+    partial class GraphTests
+    {
+        [DataRow("https://tspindler.de/arma/arma3/Download/unsung/csj_123_c.pbo")]
+        [DataRow("https://tspindler.de/arma/arma3/Download/unsung/%5bSP%5dChickenhawkUnsungAlpha.DaKrong.pbo")]
+        [DataRow("https://tspindler.de/arma/arma3/Download/unsung/%5bSP%5dChickenhawk.DaKrong.pbo")]
+        [DataRow("https://tspindler.de/arma/arma3/Download/unsung/uns_woodbridge.pbo")]
+        [TestMethod]
+        public Task pbo(string source)
+        {
+            return TestOutputGraph(source);
+        }
+    }
+}
