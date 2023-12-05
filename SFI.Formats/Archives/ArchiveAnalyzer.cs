53249a3df9340ff74146ed8f86c029287277ee02
IS4 <illidans4@gmail.com>
2023-12-05 18:23:09 +0100

    Use IFileNodeInfo implementation for archives if provided

--- SFI.Formats/Archives/ArchiveAnalyzer.cs
+++ SFI.Formats/Archives/ArchiveAnalyzer.cs
@@ -2,8 +2,10 @@
 using IS4.SFI.Services;
 using IS4.SFI.Tools;
 using IS4.SFI.Vocabulary;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Security.Cryptography;
 using System.Threading.Tasks;
 
@@ -26,7 +28,7 @@ namespace IS4.SFI.Analyzers
         {
             var node = GetNode(context);
 
-            var info = new ArchiveRoot(archive);
+            var info = new ArchiveRoot(archive, archive as IFileNodeInfo);
 
             var result = await analyzers.Analyze(info, context.WithNode(node));
 
@@ -42,10 +44,12 @@ namespace IS4.SFI.Analyzers
         class ArchiveRoot : RootDirectoryInfo
         {
             readonly IArchiveFile archive;
+            readonly IFileNodeInfo? baseInfo;
 
-            public ArchiveRoot(IArchiveFile archive)
+            public ArchiveRoot(IArchiveFile archive, IFileNodeInfo? baseInfo)
             {
                 this.archive = archive;
+                this.baseInfo = baseInfo;
             }
 
             public override IEnumerable<IFileNodeInfo> Entries => archive.Entries;
@@ -53,6 +57,22 @@ namespace IS4.SFI.Analyzers
             public override object? ReferenceKey => archive;
 
             public override object? DataKey => null;
+
+            public override FileAttributes Attributes => baseInfo?.Attributes ?? base.Attributes;
+
+            public override DateTime? CreationTime => baseInfo?.CreationTime ?? base.CreationTime;
+
+            public override DateTime? LastAccessTime => baseInfo?.LastAccessTime ?? base.LastAccessTime;
+
+            public override DateTime? LastWriteTime => baseInfo?.LastWriteTime ?? base.LastWriteTime;
+
+            public override string? Name => baseInfo?.Name ?? base.Name;
+
+            public override string Path => baseInfo?.Path ?? base.Path;
+
+            public override int? Revision => baseInfo?.Revision ?? base.Revision;
+
+            public override string? SubName => baseInfo?.SubName ?? base.SubName;
         }
     }
 }
