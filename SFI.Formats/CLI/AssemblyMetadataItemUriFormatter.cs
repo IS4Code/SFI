addbece721f156eeab1be12854ad01ee1e46eecd
IS4 <illidans4@gmail.com>
2024-02-04 14:24:35 +0100

    extend with Namespace support

--- SFI.Formats/CLI/AssemblyMetadataItemUriFormatter.cs
+++ SFI.Formats/CLI/AssemblyMetadataItemUriFormatter.cs
@@ -1,5 +1,4 @@
-﻿using IS4.SFI.Services;
-using System;
+﻿using System;
 using System.Reflection;
 
 namespace IS4.SFI.Services
@@ -7,19 +6,20 @@ namespace IS4.SFI.Services
     /// <summary>
     /// Provides support for formatting assemblies and types as URIs.
     /// </summary>
-    public class AssemblyMetadataItemUriFormatter : IIndividualUriFormatter<AssemblyName>, IIndividualUriFormatter<Type>
+    public class AssemblyMetadataItemUriFormatter : IIndividualUriFormatter<Assembly>, IIndividualUriFormatter<AssemblyName>, IIndividualUriFormatter<Namespace>, IIndividualUriFormatter<Type>
     {
         public static readonly AssemblyMetadataItemUriFormatter Instance = new();
 
-        /// <summary>
-        /// Creates a new instance of the class.
-        /// </summary>
-        public AssemblyMetadataItemUriFormatter()
+        AssemblyMetadataItemUriFormatter()
         {
 
         }
 
+        Uri IUriFormatter<Assembly>.this[Assembly value] => new($"clr-namespace:;assembly={value.GetName().Name}");
+
         Uri IUriFormatter<AssemblyName>.this[AssemblyName value] => new($"clr-namespace:;assembly={value.Name}");
+
+        Uri IUriFormatter<Namespace>.this[Namespace value] => new($"clr-namespace:{value.FullName};assembly={value.Assembly.GetName().Name}");
 
         Uri IUriFormatter<Type>.this[Type value] => new($"clr-namespace:{value.Namespace};assembly={value.Assembly.GetName().Name}#{value.Name}");
     }
