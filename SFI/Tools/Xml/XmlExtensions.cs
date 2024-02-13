d1cdc1920b00e3c5fd82eafa4856968c5d042fea
IS4 <illidans4@gmail.com>
2024-02-13 18:53:19 +0100

    XmlReader.CopyTo(Async)

--- SFI/Tools/Xml/XmlExtensions.cs
+++ SFI/Tools/Xml/XmlExtensions.cs
@@ -14,9 +14,21 @@ namespace IS4.SFI.Tools.Xml
         /// </summary>
         /// <param name="document">The XML document instance to use.</param>
         /// <param name="reader">The XML reader to read the data from.</param>
-        public static async Task LoadAsync(this XmlDocument document, XmlReader reader)
+        public static async ValueTask LoadAsync(this XmlDocument document, XmlReader reader)
         {
             using XmlWriter writer = document.CreateNavigator().AppendChild();
+            await CopyToAsync(reader, writer);
+        }
+
+        /// <summary>
+        /// Asynchronously copies the contents of an <see cref="XmlReader"/>
+        /// instance to an <see cref="XmlWriter"/>.
+        /// </summary>
+        /// <param name="reader">The XML reader to read the data from.</param>
+        /// <param name="writer">The XML writer to copy the data to.</param>
+        /// <remarks>Nodes of type <see cref="XmlNodeType.DocumentType"/> and <see cref="XmlNodeType.XmlDeclaration"/> are not copied.</remarks>
+        public static async ValueTask CopyToAsync(this XmlReader reader, XmlWriter writer)
+        {
             do{
                 switch(reader.NodeType)
                 {
@@ -38,7 +50,7 @@ namespace IS4.SFI.Tools.Xml
                         writer.WriteEntityRef(reader.Name);
                         break;
                     case XmlNodeType.ProcessingInstruction:
-                        writer.WriteProcessingInstruction(reader.Name, reader.Value);
+                        writer.WriteProcessingInstruction(reader.Name, await reader.GetValueAsync());
                         break;
                     case XmlNodeType.Comment:
                         writer.WriteComment(reader.Value);
@@ -52,6 +64,53 @@ namespace IS4.SFI.Tools.Xml
                         break;
                 }
             }while(await reader.ReadAsync());
+        }
+
+
+        /// <summary>
+        /// Copies the contents of an <see cref="XmlReader"/>
+        /// instance to an <see cref="XmlWriter"/>.
+        /// </summary>
+        /// <param name="reader">The XML reader to read the data from.</param>
+        /// <param name="writer">The XML writer to copy the data to.</param>
+        /// <remarks>Nodes of type <see cref="XmlNodeType.DocumentType"/> and <see cref="XmlNodeType.XmlDeclaration"/> are not copied.</remarks>
+        public static void CopyTo(this XmlReader reader, XmlWriter writer)
+        {
+            do{
+                switch(reader.NodeType)
+                {
+                    case XmlNodeType.Element:
+                        writer.WriteStartElement(reader.Prefix, reader.LocalName, reader.NamespaceURI);
+                        writer.WriteAttributes(reader, true);
+                        if(reader.IsEmptyElement)
+                        {
+                            writer.WriteEndElement();
+                        }
+                        break;
+                    case XmlNodeType.Text:
+                        writer.WriteString(reader.Value);
+                        break;
+                    case XmlNodeType.CDATA:
+                        writer.WriteCData(reader.Value);
+                        break;
+                    case XmlNodeType.EntityReference:
+                        writer.WriteEntityRef(reader.Name);
+                        break;
+                    case XmlNodeType.ProcessingInstruction:
+                        writer.WriteProcessingInstruction(reader.Name, reader.Value);
+                        break;
+                    case XmlNodeType.Comment:
+                        writer.WriteComment(reader.Value);
+                        break;
+                    case XmlNodeType.Whitespace:
+                    case XmlNodeType.SignificantWhitespace:
+                        writer.WriteWhitespace(reader.Value);
+                        break;
+                    case XmlNodeType.EndElement:
+                        writer.WriteFullEndElement();
+                        break;
+                }
+            }while(reader.Read());
         }
     }
 }
